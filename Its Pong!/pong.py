68f5cdc9ce772956f6349d4925de26a12d987bbd
Cameron Nelson <cameronnelson550@gmail.com>
2018-03-19 17:30:57 -0700

    Update pong.py

--- Its Pong!/pong.py
+++ Its Pong!/pong.py
@@ -1,3 +1,4 @@
+
 import pygame
 import time
 import random
@@ -14,6 +15,9 @@ clock = pygame.time.Clock()
          # display settings
 gamedisplay = pygame.display.set_mode((displaywidth,displayheight))
 pygame.display.set_caption("It's Pong!")
+
+def update():
+    pygame.display.update()
          #
 class images:
 
@@ -23,8 +27,18 @@ class images:
         
         self.itspongimg = "its pong!!.png"
         self.itspong = pygame.image.load(self.itspongimg)
-        
 
+        self.gameoverimgload = "gameover.png"
+        self.gameoverimg = pygame.image.load(self.gameoverimgload)
+
+        self.score1img1 = "scoreimg1.png"
+        self.score2img1 = "scoreimg2.png"
+
+        self.score1img = pygame.image.load(self.score1img1)
+        self.score2img = pygame.image.load(self.score2img1)
+        
+        
+        
     def line(self):
         gamedisplay.blit(self.theline,(-5,70))
 
@@ -32,6 +46,20 @@ class images:
 
     def its_pong(self):
         gamedisplay.blit(self.itspong, (-110,-70))
+
+    def itsplayerscore(self):
+        gamedisplay.blit(self.score1img, (425, 5))
+
+    def itsAIscore(self):
+        gamedisplay.blit(self.score2img, (495, 35))
+        
+
+    def gameoverimage(self):
+        gamedisplay.blit(self.gameoverimg, (220,200))
+
+
+
+       
         
         
 class ball:
@@ -56,9 +84,6 @@ class ball:
         
 
     def b_imgblit(self):
-        
-        
-        
         gamedisplay.blit(self.ballimg, (self.x,self.y))
         
         
@@ -67,12 +92,12 @@ class ball:
         self.y = self.y + self.ys
 
 
-
+  
 
     def detectpaddle(self):
         #Im sure theres a better solution to detect the paddle,
         #but this is the first one i could think of.
-        self.paddletop = game1paddle1.paddle1y + 58
+        self.paddletop = game1paddle1.paddle1y + 55
         self.paddlebottom = game1paddle1.paddle1y - 58
         self.aipaddletop = ai_1.paddle2y + 58
         self.aipaddlebottom = ai_1.paddle2y - 58
@@ -115,8 +140,6 @@ class player:
         
         
     def paddle1(self):
-
-
 
          #paddle borders
        if self.paddle1y <= displayheight * 0.15:
@@ -192,13 +215,86 @@ class AI():
            self.paddle2y = displayheight * 0.799
 
 
-   
+class gamelogic:
+    def __init__(self):
+        
+        self.player1score = 0
+        self.ai_score = 0
+        self.pscoreimg = "0.png"
+        self.aiscoreimg = "0.png"
+        self.imglist =  ("1.png", "2.png", "3.png", "4.png", "5.png")
+        
+    def ballreset(self):
+        if ball1.x > 800 or ball1.x < 0:
+            ball1.x = 400
+            ball1.y = 300
+            ball1.xs = ball1.xs * -1
+            time.sleep(1.5)
 
-            
         
-def update():
-    pygame.display.update()
+    def playerscore(self):
+        #adds point if ball passes behind paddle
+        if ball1.x < 2:
+            self.ai_score = self.ai_score + 1
+
+        elif ball1.x > 798:
+            self.player1score = self.player1score + 1
+        #
+        #loads score image relevant to the score
+        if self.player1score == 1:
+            self.pscoreimg = self.imglist[0]
+
+        elif self.player1score == 2:
+            self.pscoreimg = self.imglist[1]
+
+        elif self.player1score == 3:
+            self.pscoreimg = self.imglist[2]
+
+        elif self.player1score == 4:
+            self.pscoreimg = self.imglist[3]
+
+        elif self.player1score == 5:
+            self.pscoreimg = self.imglist[4]
+
+        else:
+            self.pscoreimg = "0.png"
         
+        gamedisplay.blit(pygame.image.load(self.pscoreimg), (650,2))
+        
+
+        if self.ai_score == 1:
+            self.aiscoreimg = self.imglist[0]
+
+        elif self.ai_score == 2:
+            self.aiscoreimg = self.imglist[1]
+
+        elif self.ai_score == 3:
+            self.aiscoreimg = self.imglist[2]
+
+        elif self.ai_score == 4:
+            self.aiscoreimg = self.imglist[3]
+
+        elif self.ai_score == 5:
+            self.aiscoreimg = self.imglist[4]
+
+        else:
+            self.aiscoreimg = "0.png"
+
+        
+        gamedisplay.blit(pygame.image.load(self.aiscoreimg),(650,32))
+        #
+    
+    def gameover(self):
+        if self.player1score == 5 or self.ai_score == 5:
+            self.player1score = 0
+            self.ai_score = 0
+
+            image1.gameoverimage()
+            update()
+            time.sleep(5)
+                
+                
+
         
 
 #class instances
@@ -207,6 +303,7 @@ ball1 = ball()
 image1 = images()
 game1paddle1 = player()
 ai_1 = AI()
+game1 = gamelogic()
 #    
 
 
@@ -222,31 +319,36 @@ while not crashed:
     #imagefunctions
     image1.its_pong()
     image1.line()
+    image1.itsplayerscore()
+    image1.itsAIscore()
     #
     
     
     #ball functions
     ball1.b_imgblit()
     ball1.ballmove()
+    
     ball1.detectborder()
     ball1.detectpaddle()
     
    
     #player functions    
     game1paddle1.paddle1()
-    
+
+    #ai functions
     ai_1.AIdetectborder()
     ai_1.paddleai()
     ai_1.paddle2blit()
-    
+
+    #gamelogic functions
+    game1.ballreset()
+    game1.playerscore()
+    game1.gameover()
     
     update()
   
-
-    
     gamedisplay.fill(white)
   
-
     clock.tick(60)
 
 
